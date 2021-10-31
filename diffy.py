6e548e785ee25d23b18ca35ee1b7d28aebfe2269
Joshua Conrad <jozbornn@gmail.com>
2021-10-31 15:02:19 -0700

    DPSSTRegister refactor
    Added sort method
    Added sum (opposite of diff)
    Added save
    Added sort keys to DPSSTGroup
    Added version "1" to Entry

--- diffy.py
+++ diffy.py
@@ -7,9 +7,15 @@ class Entry:
 
     def __init__(self, data):
         data = data.split("\t")
+
         self.name = data[0]
         self.id = data[1]
-        self.agency = data[2]
+
+        self.version = "1"
+
+        if len(data) > 2:
+            self.agency = data[2]
+            self.version = "old"
 
         if len(data) > 3:
             # For backwards compatibility's sake
@@ -19,30 +25,33 @@ class Entry:
             self.status = data[4]
 
             self.version = "new"
-        else:
-            self.version = "old"
 
     def __eq__(self, other):
         if not isinstance(other, Entry):
             return False
         else:
-            if other.version == "new":
+            if other.version == "1":
+                return self.name == other.name \
+                    and self.id == other.id
+            elif other.version == "old":
+                return self.name == other.name \
+                    and self.id == other.id \
+                    and self.agency == other.agency
+            elif other.version == "new":
                 # Do all parts match?
                 # Not including status because it's not really relevant and is bugged
                 return self.name == other.name \
                     and self.id == other.id \
                     and self.agency == other.agency \
                     and self.rank == other.rank
-            else:
-                return self.name == other.name \
-                    and self.id == other.id \
-                    and self.agency == other.agency
 
     def __str__(self):
-        if self.version == "new":
-            return "\t".join([self.name, self.id, self.agency, self.rank, self.status])
-        else:
+        if self.version == "1":
+            return self.name + "\t" + self.id
+        elif self.version == "old":
             return "\t".join([self.name, self.id, self.agency])
+        elif self.version == "new":
+            return "\t".join([self.name, self.id, self.agency, self.rank, self.status])
 
 
 class EntryList:
@@ -101,6 +110,14 @@ class DPSSTGroup:
 
     def __str__(self):
         return self.name + "\t" + self.dpsst_num
+
+    @staticmethod
+    def sort_by_dpsst(group):
+        return group.dpsst_num
+
+    @staticmethod
+    def sort_by_name(group):
+        return group.name.lower()
 
     def append(self, entry: Entry):
         if entry.id == self.dpsst_num:
@@ -166,7 +183,7 @@ class DPSSTRegister:
         else:
             self.groups.append(DPSSTGroup(entry.id, [entry]))
 
-    def diff(self, other):
+    def diff(self, other, log=True):
         """
         If self is A and other is B, then:
         A ~ B =
@@ -176,18 +193,25 @@ class DPSSTRegister:
         In order for differencing to work properly, A must be from the earlier time,
         and B from the later time. Otherwise the results will be reversed
         """
-        new = []
-        for other_group in other:
-            found = False
-            for self_group in self:
-                # Only checks for the matching DPSST number, not matching entries
-                if other_group.dpsst_num == self_group.dpsst_num:
-                    self.groups.pop(self.groups.index(self_group))
-                    found = True
-                    break
-            if not found:
-                new.append(other_group)
-        return [self, DPSSTRegister(new)]
+        if not isinstance(other, DPSSTRegister):
+            raise ValueError(f"Cannot difference {type(other)}, only DPSSTRegister")
+        else:
+            new = []
+            if log:
+                print("Differencing DPSSTRegisters...")
+            for other_group in other:
+                found = False
+                for self_group in self:
+                    # Only checks for the matching DPSST number, not matching entries
+                    if other_group.dpsst_num == self_group.dpsst_num:
+                        self.groups.pop(self.groups.index(self_group))
+                        found = True
+                        break
+                if not found:
+                    new.append(other_group)
+            if log:
+                print("Differencing complete.")
+            return [self, DPSSTRegister(new)]
 
     def get(self, dpsst_num):
         for each in self:
@@ -196,25 +220,59 @@ class DPSSTRegister:
 
         return None
 
+    def save(self, filename, log=True):
+        with open(filename, "w+") as f:
+            f.write(str(self))
 
-def build_and_diff_dpsst_registers(datecode0, datecode1):
+    def sort(self, by="dpsst"):
+        if by == "name":
+            self.groups.sort(key=DPSSTGroup.sort_by_name)
+        elif by == "dpsst":
+            self.groups.sort(key=DPSSTGroup.sort_by_dpsst)
+
+    def sum(self, other):
+        if not isinstance(other, DPSSTRegister):
+            raise ValueError(f"Cannot sum {type(other)}, only DPSSTRegister")
+        else:
+            for each in other:
+                g = self.get(each.dpsst_num)
+                if not g:
+                    self.groups.append(each)
+                else:
+                    for entry in each:
+                        self.add(entry)
+
+
+def build_and_diff_dpsst_registers(datecode0, datecode1, directory="coplist_tsv", log=True):
 
     # Load registers
-    reg1 = DPSSTRegister.open(f"coplist_tsv/{datecode0}.tsv")
-    reg2 = DPSSTRegister.open(f"coplist_tsv/{datecode1}.tsv")
+    if log:
+        print("Loading registers for differencing...")
+
+    reg1 = DPSSTRegister.open(f"{directory}/{datecode0}.tsv", log=log)
+    reg2 = DPSSTRegister.open(f"{directory}/{datecode1}.tsv", log=log)
 
     # Diff registers
-    diff = reg1.diff(reg2)
+    diff = reg1.diff(reg2, log=log)
 
     # Save diff results
+    if log:
+        print("Saving differenced register...")
+
     for i in range(2):
         if i == 0:
             diff_type = "removed"
         else:
             diff_type = "added"
-        with open(f"coplist_tsv/{datecode0}-{datecode1}-{diff_type}.txt", "w+") as f:
+
+        with open(f"{directory}/{datecode0}-{datecode1}-{diff_type}.tsv", "w+") as f:
             f.write(str(diff[i]))
+
+        if log:
+            print(f"Saved {datecode0}-{datecode1}-{diff_type}.tsv")
 
 
 if __name__ == "__main__":
-    build_and_diff_dpsst_registers("20211029", "20211030")
+    # build_and_diff_dpsst_registers("20200625", "20211029")
+    # status_test()
+    pass
