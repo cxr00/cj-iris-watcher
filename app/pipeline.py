cf1ca54f6a2a2cccaa104f2b27fa4b55749b1fae
Joshua Conrad <jozbornn@gmail.com>
2021-11-10 15:22:17 -0800

    Add directory parameter to pipeline methods
    You still need a valid repo, but I haven't made DiffEntryHistory.save() yet

--- app/pipeline.py
+++ app/pipeline.py
@@ -5,12 +5,15 @@ import datetime
 import os
 
 
-def scrape_and_diff_today_from_yesterday():
+def scrape_and_diff_today_from_yesterday(directory=None):
     # Construct datecodes
     today = datetime.datetime.now()
 
+    if not directory:
+        directory = "repo"
+
     # Get previous day from _meta file
-    with open(f"repo/diff/_meta.txt", "r") as f:
+    with open(f"{directory}/diff/_meta.txt", "r") as f:
         datecode0 = f.readlines()[-1].strip()
 
     datecode1 = today.strftime("%Y%m%d")
@@ -20,15 +23,15 @@ def scrape_and_diff_today_from_yesterday():
         return
 
     # Scrape from BPL site
-    if not os.path.exists(f"repo/scrape/{datecode1}.tsv"):
+    if not os.path.exists(f"{directory}/scrape/{datecode1}.tsv"):
         print("Scraping data...")
         scrape_all_data(datecode=datecode1, directory="repo/scrape")
     else:
         print("Today's data has already been scraped.")
 
     # Get EntryLists for differencing
-    el0 = DiffEntryHistory.open(f"repo/diff").rebuild()
-    el1 = EntryList.open(f"repo/scrape/{datecode1}.tsv")
+    el0 = DiffEntryHistory.open(f"{directory}/diff").rebuild()
+    el1 = EntryList.open(f"{directory}/scrape/{datecode1}.tsv")
     diff = el0.diff(el1, log=True)
 
     print(f"Removed since {datecode0[4:6]}/{datecode0[6:]}/{datecode0[:4]} (-):")
@@ -38,7 +41,7 @@ def scrape_and_diff_today_from_yesterday():
     print(f"Added to {datecode1[4:6]}/{datecode1[6:]}/{datecode1[:4]} (+):")
     print(str(diff[1]))
 
-    diff_file = f"repo/diff/{datecode0}-{datecode1}.tsv"
+    diff_file = f"{directory}/diff/{datecode0}-{datecode1}.tsv"
 
     report_string = ""
 
@@ -54,13 +57,13 @@ def scrape_and_diff_today_from_yesterday():
     print(f"Difference saved to {diff_file}")
 
     # Update diff _meta file with new day
-    with open(f"repo/diff/_meta.txt", "a+") as f:
+    with open(f"{directory}/diff/_meta.txt", "a+") as f:
         f.write(datecode1 + "\n")
 
     print(f"Meta file updated with {datecode1}")
 
 
-def delete_old_tsv():
+def delete_old_tsv(directory=None):
     """
     Deletes TSV files that are no longer necessary thanks to differencing
 
@@ -69,19 +72,25 @@ def delete_old_tsv():
     def create_filename(datecode0, datecode1):
         return f"repo/diff/{datecode0}-{datecode1}.tsv"
 
-    eh = DiffEntryHistory.open("repo/diff")
+    if not directory:
+        directory = "repo"
+
+    eh = DiffEntryHistory.open(f"{directory}/diff")
 
     for n in range(1, len(eh.meta) - 2):
         diff_filename = create_filename(eh.meta[n-1], eh.meta[n])
         if os.path.exists(diff_filename):
-            del_filename = f"repo/scrape/{eh.meta[n]}.tsv"
+            del_filename = f"{directory}/scrape/{eh.meta[n]}.tsv"
             if os.path.exists(del_filename):
                 print(f"Deleting old TSV for {eh.meta[n]}")
                 os.remove(del_filename)
 
 
-def test_partial_rebuilds():
-    diff_eh = DiffEntryHistory.open("repo/diff")
+def test_partial_rebuilds(directory=None):
+    if not directory:
+        directory = "repo"
+
+    diff_eh = DiffEntryHistory.open(f"{directory}/diff")
 
     el0 = diff_eh.rebuild(n=0)
     el1 = diff_eh.rebuild(n=1)
@@ -93,9 +102,11 @@ def test_partial_rebuilds():
     print()
 
 
-def test_history_slicing():
+def test_history_slicing(directory=None):
+    if not directory:
+        directory = "repo"
 
-    diff_eh = DiffEntryHistory.open("repo/diff")
+    diff_eh = DiffEntryHistory.open(f"{directory}/diff")
 
     print("\n".join(diff_eh.meta) + "\n")
 
