65179fd9a5ca9bca8de3bf208de34c3dd26b84e4
Joshua Conrad <jozbornn@gmail.com>
2021-11-15 15:09:23 -0800

    Removed unused import

--- app/bpl_scraper.py
+++ app/bpl_scraper.py
@@ -4,7 +4,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-import time
 import re
 import os
 
