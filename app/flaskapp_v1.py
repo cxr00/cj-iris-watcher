7bdff521b058f422e3d77c1b0ca591be0d84bf5d
Joshua Conrad <jozbornn@gmail.com>
2022-09-02 10:53:26 -0700

    Now I get it

--- app/flaskapp_v1.py
+++ app/flaskapp_v1.py
@@ -94,4 +94,4 @@ def legacy_portal():
 
 if __name__ == "__main__":
     print(f"Serving CJ IRIS Watcher at 127.0.0.1:5000")
-    serve(app, host="0.0.0.0", port=5000)
+    serve(app, host="127.0.0.1", port=5000)
